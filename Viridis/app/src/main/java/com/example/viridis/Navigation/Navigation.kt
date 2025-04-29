e8e1ccbcc8debe6d3d09f395869d9ab5e70d9fbe
AlexE20 <00009423@uca.edu.sv>
2025-04-29 00:44:11 -0600

    Fixing navigation logic

---
+++ Viridis/app/src/main/java/com/example/viridis/Navigation/Navigation.kt
@@ -0,0 +1,12 @@
+package com.example.viridis.Navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+
+sealed class Navigation(val route:String){
+@Serializable
+object Meeting: Navigation("meeting")
+    @Serializable
+    object Notification: Navigation("notification")
+}
