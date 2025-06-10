d0800b06b4820ce990fdcbf9531da3bf3b8b6c26
JuanPayes <00005923@uca.edu.sv>
2025-06-10 14:04:47 -0600

    Adding SearchScrean

--- Viridis/app/src/main/java/com/example/viridis/ui/components/CustomTopBar.kt
+++ Viridis/app/src/main/java/com/example/viridis/ui/components/CustomTopBar.kt
@@ -1,10 +1,12 @@
 package com.example.viridis.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -56,7 +58,7 @@ fun CustomTopBar(
         Column(
             modifier = Modifier
                 .padding(innerPadding)
-                .fillMaxSize()
+                .fillMaxSize().background(BackgroundColor)
         ) {
             content()
         }
