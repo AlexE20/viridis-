cdba80da3e153ed43c228f667476a7a1a6c9a515
AlexE20 <00009423@uca.edu.sv>
2025-06-22 14:54:32 -0600

    :construction: Working on plant repository

--- Viridis/app/src/main/java/com/pdm/viridis/data/repository/Plant/PlantRepositoryImpl.kt
+++ Viridis/app/src/main/java/com/pdm/viridis/data/repository/Plant/PlantRepositoryImpl.kt
@@ -10,36 +10,34 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 class PlantRepositoryImpl(
-    private val plantService: PlantService
+	private val plantService: PlantService
 ) : PlantRepository {
-
-
-
-    override suspend fun getCatalogPlants(limit: Int?, startAfter: String?): List<Plant> {
-        return try {
-            plantService.getCatalogPlants(
-                limit = limit ?: 20,
-                startAfter = startAfter
-            )
-        } catch (e: retrofit2.HttpException) {
-            if (e.code() == 404) {
-                emptyList()
-            } else {
-                throw e
-            }
-        }
-    }
-
-    override suspend fun getCatalogPlantsByName(name: String): List<Plant> {
-        return try {
-            plantService.getCatalogPlantsByName(name)
-
-        } catch (e: retrofit2.HttpException) {
-
-            throw e
-
-        }
-    }
-
-
+	
+	
+	override suspend fun getCatalogPlants(limit: Int?, startAfter: String?): List<Plant> {
+		return try {
+			plantService.getCatalogPlants(
+				limit = limit ?: 20, startAfter = startAfter
+			)
+		} catch (e: retrofit2.HttpException) {
+			if (e.code() == 404) {
+				emptyList()
+			} else {
+				throw e
+			}
+		}
+	}
+	
+	override suspend fun getCatalogPlantsByName(name: String): List<Plant> {
+		return try {
+			plantService.getCatalogPlantsByName(name)
+			
+		} catch (e: retrofit2.HttpException) {
+			
+			throw e
+			
+		}
+	}
+	
+	
 }
